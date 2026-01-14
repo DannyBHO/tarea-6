90459942b25f1020ccfe7183e8077136a661d74b
DannyBHO <do.banchonh@uea.edu.ec>
2026-01-14 17:06:30 -0500

    Delete Sistema de empleos/empleados directory

--- Sistema de empleos/empleados/servicios/gestion.py
+++
@@ -1,7 +0,0 @@
-def mostrar_beneficio(empleado):
-    """
-    Función que recibe un objeto Empleado
-    y aplica polimorfismo
-    """
-    print(f"Empleado: {empleado.get_nombre()}")
-    print(f"Beneficio: ${empleado.calcular_beneficio():.2f}")
